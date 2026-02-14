8c7047d160d63b6dcaba9fbc28f54dfca43d54e1
Pavan <pavanmamidi1432@gmail.com>
2026-02-14 17:35:48 +0530

    Disable password validators for easier development/testing

--- mediavault_project/settings.py
+++ mediavault_project/settings.py
@@ -80,12 +80,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 # --- STATIC & MEDIA FILES ---
 STATIC_URL = '/static/'
